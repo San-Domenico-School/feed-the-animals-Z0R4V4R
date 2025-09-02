19a77aec53077deabc315fbc9e13aee35ae31654
Z0R4V4R <znijjar28@sandomenico.org>
2025-09-02 09:09:49 -0700

    feed the animals

--- Assets/Scipts/FeedTheAnimals/FeedTheAnimals.cs
+++ Assets/Scipts/FeedTheAnimals/FeedTheAnimals.cs
@@ -1,3 +1,16 @@
+/******************************************************************************  
+ * Class: FeedTheAnimals
+ * Purpose: Controls player movement based on input.  
+ * Component Of: Player GameObject  
+ * Fields: 
+ *   - Food (float) 
+ * Behaviors:
+ *   - OnFeedAnimalEnter
+ *   - FeedAnimal
+ * Author: Zo Nijjar
+ * Version: August, 2025 v. 1.0
+ *******************************************************************************/
+
 using UnityEngine;
 
 public class FeedTheAnimals
